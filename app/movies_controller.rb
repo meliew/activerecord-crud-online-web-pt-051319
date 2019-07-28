42583d7648838686105b0317924a725415e5fd5c
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 03:16:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 
 end
 
-def can_be_created_in_a_block(args= )
+def can_be_created_in_a_block(args )
   Movie.create do |m|
   m.title = "Home Alone"
   m.release_date = 1990
