738f9be5a86b3c6f51fab5d3fbcbf92370e60e15
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 03:07:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,12 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new do |a|
-  a.title = "The Sting"
-  a.release_date = 1973
-  a.director = "George Roy Hill"
-  a.lead = "Paul Newman"
-  a.in_theaters = false
+  movie = Movie.create(attributes)
   end
 end
 
