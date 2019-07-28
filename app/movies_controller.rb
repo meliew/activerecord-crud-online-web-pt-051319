b66254345c218852355e88bf327d738a9600362a
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 03:12:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ def can_be_created_with_a_hash_of_attributes
 
 end
 
-def can_be_created_in_a_block(args = )
-  movie = Movie.create do |a|
+def can_be_created_in_a_block
+  Movie.create do |a|
   a.title = "Home Alone"
   a.release_date = 1990
   # If no arguments are passed, use default values:
